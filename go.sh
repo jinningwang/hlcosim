b064587d118960298d1ef888f2d1d497fb2ad5d4
jinningwang <jinninggm@gmail.com>
2023-03-14 21:07:46 -0400

    Rename

---
+++ go.sh
@@ -0,0 +1,25 @@
+
+build{
+    echo "Current environment is: $CONDA_DEFAULT_ENV"
+
+    echo "Installate dependencies ..."
+
+    conda install --file requirements.txt
+
+    echo "Clone LTB packages ..."
+
+    git clone https://github.com/CURENT/ltb.git --recursive
+
+    echo "Install LTB DiME ..."
+    cd $HOME/hlcosim/ltb/dime/server
+    make
+    make install
+
+    echo "Initialize LTB ANDES ..."
+    cd $HOME/hlcosim
+    andes prep -f
+
+    if [ -f $HOME/.andes/andes.rc ]; then rm $HOME/.andes/andes.rc; fi
+    cp $HOME/hlcosim/ltb/andes.rc $HOME/.andes/
+    echo "Removed default andes config and set the given config."
+}
