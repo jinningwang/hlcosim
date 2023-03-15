4960806e4a8059507755ba71e3ebc0da9252fdcf
jinningwang <jinninggm@gmail.com>
2023-03-14 21:42:40 -0400

    Fix

--- go.sh
+++ go.sh
@@ -1,7 +1,11 @@
 
 #!/bin/bash
 
-function build {
+say() {
+    echo "HELLO"
+}
+
+build() {
     echo "Current environment is: $CONDA_DEFAULT_ENV"
 
     echo "Installate dependencies ..."
@@ -19,31 +23,32 @@ function build {
 
     echo "Initialize LTB ANDES ..."
     cd $HOME/hlcosim
-    andes prep -f
+    andes prep
 
     if [ -f $HOME/.andes/andes.rc ]; then rm $HOME/.andes/andes.rc; fi
-    cp $HOME/hlcosim/ltb/andes.rc $HOME/.andes/
+    cp $HOME/hlcosim/code/andes.rc $HOME/.andes/
     echo "Removed default andes config and set the given config."
 }
 
-function clean {
+clean() {
     echo "Current environment is: $CONDA_DEFAULT_ENV"
 
     echo "Remove LTB packages ..."
-    if [ -f $HOME/hlcosim/ltb/ ]; then rm -rf $HOME/hlcosim/ltb; fi
+    if [ -d $HOME/hlcosim/ltb/ ]; then rm -rf $HOME/hlcosim/ltb; fi
 
     echo "Remove LTB DiME file ..."
     if [ -f tmp/dime2 ]; then rm tmp/dime2; fi
 }
 
-function dime {
-    sudo dime -l unix:/tmp/dime2 -l unix:/var/run/dime.sock -l ws:8818 -vv
-}
-
-function agvis {
+sweb() {
+    echo "Start the web ..."
     python3 -m http.server -d ./static 8810 --bind 0.0.0.0
 }
 
-function jupyter {
+sdime() {
+    dime -l unix:/tmp/dime2 -l unix:/var/run/dime.sock -l ws:8818 -vv
+}
+
+sjupyter() {
     sudo -E env "PATH=$PATH" jupyter notebook --allow-root
 }
