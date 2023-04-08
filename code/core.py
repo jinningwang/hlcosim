70d5574e881b59b3a1613d0ad5418916cd85be74
jinningwang <jinninggm@gmail.com>
2023-04-08 14:08:15 -0400

    Improve code

--- code/core.py
+++ code/core.py
@@ -5,6 +5,7 @@ import os
 import logging
 import csv
 import time
+from collections import OrderedDict
 
 import numpy as np
 import pandas as pd
@@ -14,117 +15,156 @@ andes.config_logger(stream_level=50)
 
 logger = logging.getLogger(__name__)
 
-io_config = dict(k_df=-4, p_df=0, q_df=0, htb_s=1e4, htb_b=-2)
-
-status = dict(iter_total=0, iter_fail=0,
-               kr=-1, k=0,
-               v=0, freq=0, p=0, q=0, 
-               tw=0, tr=0, tsim=0, tf=0,)
-
-config = dict(ti=1, t_step=0.05,
-                 itermax_io=20, load_switch=True)
+status = OrderedDict([
+        ('iter_total', 0), ('iter_fail', 0),
+        ('kr', 0), ('k', 0),
+        ('v', 0), ('freq', 0), ('p', 0), ('q', 0),
+        ('pdef', 0), ('qdef', 0),
+        ('tw', 0), ('tr', 0), ('tsim', 0), ('tf', 0),
+        ])
 
 scols = list(status.keys())[2:]
 
-def data_read(file, config=io_config):
-    """
-    Read data from a txt file.
 
-    ``k``, ``p``, and ``q`` are the counter, active power,
-    and reactive power, respectively.
+def data_io(file, data=None, pdef=None, qdef=None):
+    """
+    Read or write data from/to a txt file.
+
+    Read data [k, p, q] from file if data is None,
+    otherwise write data [freq, volt] to file.
+
+    The read data is converted from HEX to DEC, and converted by linear
+    equations as follows:
+
+    p = p / k_htb + b_htb
+
+    q = q / k_htb + b_htb
 
     Parameters
     ------------
     file: str
-        Name of the file to read
-    config: dict
-        Configuration dictionary
+        Name of the file to read or write
+    data: list or None
+        List of data to write, or None if reading data
+    pdef: float or None
+        Default value of p, will be updated if data is read successfully
+    qdef: float or None
+        Default value of q, will be updated if data is read successfully
 
     Returns
     ---------
-    out: list
-        List of read data, [k, p, q]
-    txtc: str
-        Raw text read from file
-    io_flag: bool
-        Flag to indicate if data reading is successful
+    out: list or None
+        List of read data, or None if writing data
+    exit_code: bool
+        Flag to indicate if data reading or writing is successful
+
+    Config
+    ---------
+    kdef: int
+        Default value of read counter value k
+    k_htb: float
+        Conversion factor
+    b_htb: float
+        Conversion bias
     """
-    [k_df, p_df, q_df] = [config['k_df'], config['p_df'],
-                          config['q_df']]
-    io_flag = False
+    kdef=-4
+    k_htb=1e4
+    b_htb=-2
+
+    exit_code = False
     try:
-        txtr = open(file)
-        txtc = txtr.read()
-        txtr.close()
-        # HEX to DEC  # NOTE: previous version: [k, p, q] = [int(i, 10) for i in txtc.split()]
-        [k, p, q] = [int(i, 10) for i in txtc.split()]
-        # --- data conversion ---
-        p = p / config['htb_s'] + config['htb_b']
-        q = q / config['htb_s'] + config['htb_b']
-        out = [k, p, q]
-        io_flag = True
-        msg = "Data read from %s: k=%d, p=%f, q=%f" % (file, k, p, q)
+        if data is None:  # read data from file
+            txtr = open(file)
+            txtc = txtr.read()
+            txtr.close()
+            [k, p, q] = [int(i, 10) for i in txtc.split()]  # HEX to DEC
+            # --- data conversion ---
+            p = p / k_htb + b_htb
+            q = q / k_htb + b_htb
+            # --- update default values ---
+            if pdef is not None:
+                pdef = p
+            if qdef is not None:
+                qdef = q
+            out = [k, p, q]
+            exit_code = True
+            msg = "Data read from %s: k=%d, p=%f, q=%f" % (file, k, p, q)
+        else:  # write data to file
+            exit_code = True
+            out = None
+            txtc = None
+            scsv = open(file, "w")
+            writer = csv.writer(scsv)
+            writer.writerow([i * k_htb for i in data])
+            scsv.close()
+            msg = "Data write to %s: freq=%d, volt=%f" % (file, data[0], data[1])
+        logger.debug(msg)
     except FileNotFoundError:
-        out = [k_df, p_df, q_df]
-        txtc = 'ERROR: File Not Found'
-        msg = "File Not Found Error occured data read from %s error" % file
+        out = [kdef, pdef, qdef] if data is None else None
+        msg = "File Not Found Error occurred" + (" data read from %s" % file if data is None else " data write to %s" % file)
+        logger.error(msg)
     except ValueError:
-        out = [k_df, p_df, q_df]
-        txtc = 'ERROR: Value Error'
-        msg = "Value Error occured data read from %s error" % file
-    logger.info(msg)
-    return out, txtc, io_flag
+        out = [kdef, pdef, qdef] if data is None else None
+        msg = "Value Error occurred" + (" data read from %s" % file if data is None else " data write to %s" % file)
+        logger.error(msg)
+    return out, exit_code
 
 
-def data_write(dataw, file, config=io_config):
+class ACEObj:
     """
-    Write data into a txt file.
-
-    ``k``, ``p``, and ``q`` are the counter, active power,
-    and reactive power, respectively.
-
-    Parameters
-    ------------
-    dataw: list
-        list of data to write
-    file: str
-        name of the file to write
-    config: dict
-        configuration dictionary
-
-    Returns
-    ---------
-    io_flag: bool
-        Flag to indicate if data writting is successful
+    ACE error class.
     """
-    io_flag = False
-    scsv = open(file, "w")
-    writer = csv.writer(scsv)
-    writer.writerow([i * config['htb_s'] for i in dataw])
-    scsv.close()
-    io_flag = True
-    msg = "Data write to %s: freq=%d, volt=%f" % (file, dataw[0], dataw[1])
-    logger.info(msg)
-    return io_flag
+    def __init__(self, Kp=0, Ki=0, Integral=0):
+        """
+        Calculate ACE error.
+
+        AGC control is defined by a PI controller as follows:
+
+        Integral = Integral + ACEc
+
+        Raw = -(Kp*ACEc + Ki*Integral)
+
+        where ACEc is the error measured by model ACEc in ANDES,
+        and ACEp is the error measured by model ACEp in ANDES.
+
+        Parameters
+        ------------
+        Kp: float
+            Proportional gain
+        Ki: float
+            Integral gain
+        Integral: float
+            Initial ntegral value
+        """
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Integral = Integral
+        self.Raw = -(self.Ki*self.Integral)
+
+    def update(self, ACEc):
+        """
+        Update ACE error.
+
+        Parameters
+        ------------
+        ACEc: float
+            Input ACE error
+        """
+        self.Integral += ACEc
+        self.Raw = -(self.Kp*ACEc + self.Ki*self.Integral)
+        return True
 
 
 def run(case='ieee39_htb.xlsx', tf=20,
+        status=status,
         read_file = 'datar.txt', write_file = 'dataw.txt',
-        test_mode=True, AGC_control=True, **kwargs):
+        test_mode=True, AGC_control=True,
+        ):
     """
     Run the HTB-LTB co-simulation.
 
     In test mode, the counter in data IO is force updated without
     interfacing with HTB.
-
-    AGC control is defined by a PI controller as follows:
-
-    ACE_integral = ACE_integral + ACEc
-
-    ACE_raw = -(Kp*ACEc + Ki*ACE_integral)
-
-    where ACEc is the error measured by model ACEc in ANDES,
-    and ACE_raw is the ACE error sent to the control center.
 
     Parameters
     ------------
@@ -150,6 +190,17 @@ def run(case='ieee39_htb.xlsx', tf=20,
     status: dict
         Cosim status
     """
+    # --- set config ---
+    config = OrderedDict([
+        ('intv_agc', 1),
+        ('Kp', 0.005),
+        ('Ki', 0.001),
+        ('ti', 1),
+        ('t_step', 0.05),
+        ('itermax_io', 20),
+        ('load_switch', True),
+        ])
+    
     # --- set path ---
     path_proj = os.getcwd()
     path_case = os.path.join(path_proj, 'case')
@@ -168,15 +219,9 @@ def run(case='ieee39_htb.xlsx', tf=20,
                     setup=False)
 
     # --- AGC settings ---
-    intv_agc = 4  # interval of AGC
-
     tgov_idx = 'TGOV1_1'  # TurbineGov idx
 
-    # ACE vars for PI controller
-    ACE_integral = 0
-    ACE_raw = 0
-    Kp = 0.005  # 0.05
-    Ki = 0.001
+    ACE = ACEObj(Kp=config['Kp'], Ki=config['Ki'], Integral=0)
 
     #  --- HTB variables ---
     pq_htb = 'PQ_2'  # load represents for HTB
@@ -217,14 +262,7 @@ def run(case='ieee39_htb.xlsx', tf=20,
     rows = np.ceil((tf - config['ti'] + 1) / config['t_step'])
     cs_num = -1 * np.ones((int(rows), len(scols)))
 
-    const_freq = 2 * np.pi * ss.config.freq  # constant to calculate bus angle
-    flag_init = True  # Flag to indicate the very first iteration
     flag_tc0 = True  # Flag to record `tc0`
-
-    flag_datar = False  # Flag to indicate if data read is successful
-    flag_dataw = False  # Flag to indicate if data write is successful
-
-    flag_base = True  # Flag t0 update counter base
 
     # --- system initial conditions ---
 
@@ -252,20 +290,15 @@ def run(case='ieee39_htb.xlsx', tf=20,
     t0_htb = time.time()
 
     rows = tf / config['t_step']
-    k0 = 0
 
     logger.warning("LTB ready, start HTB to continue...")
 
     while (status['kr'] != 11):
         # --- repeat reading data until kr==11 ---
-        [status['kr'], status['p'], status['q']], txtc, flag_datar = data_read(file=read_path, config=io_config)
+        [status['kr'], status['p'], status['q']], _ = data_io(file=read_path)
         if test_mode:
             status['kr'] = 11  # Force set kr to 11 for test_mode test
         kr0 = status['kr']
-        k0 = status['k']
-        # --- reset io_config default values ---
-        [io_config['p_df'], io_config['q_df']] = [status['p'], status['q']]
-    flag_init = False  # Turn off init_flag after first iteration
     logger.warning("Co-sim initialized.")
     while status['k'] < rows:
         try:
@@ -275,32 +308,30 @@ def run(case='ieee39_htb.xlsx', tf=20,
             # --- data read ---
             # NOTE: repeat reading data until counter update
             iter_read = 0
-            k0 = status['k']
             # NOTE: repeat reading data until read counter `status['kr]` update
             t0_htb = time.time()
             while (status['kr'] != kr0 + 1) & (iter_read <= config['itermax_io']):
-                [status['kr'], status['p'], status['q']], txtc, flag_datar = data_read(file=read_path, config=io_config)
+                [status['kr'], status['p'], status['q']], _ = data_io(file=read_path)
+                status['pdef'], status['qdef'] = status['p'], status['q']  # update default value if read successfully
                 if test_mode:
                     status['kr'] = kr0 + 1  # Force update kr for test_mode test
-                [io_config['p_df'], io_config['q_df']] = [status['p'], status['q']]
                 iter_read += 1
             if iter_read > config['itermax_io']:
-                status['p'], status['q'] = io_config['p_df'], io_config['q_df']
+                status['p'], status['q'] = status['pdef'],  status['qdef']  # use default value if read failed
             tc1 = time.time()  # record clock time
             # NOTE: update cumulative counter if read counter update successfully
             kr0 = status['kr']
-            k0 = status['k']
             status['k'] += 1
             # --- LTB sim ---
             # --- info ---
-            if np.mod(status['k'], 200) == 0:
+            if status['k'] % 200 == 0:
                 logger.warning("LTB simulated to %ds..." % ss.TDS.config.tf)
             # --- send data to HTB ---
             # NOTE: Make sure `BusFreq` is connected to the load bus
             f_bus = ss.BusFreq.get(idx='BusFreq_HTB', src='f', attr='v')  # p.u.
             v_bus = ss.Bus.get(idx=bus_htb, src='v', attr='v')  # RMS, p.u.
             dataw = [v_bus, f_bus]  # LTB: voltage, frequency
-            data_write(dataw=dataw, file=write_path, config=io_config)
+            data_io(file=write_path, data=dataw)
             tc2 = time.time()  # send end time
             # --- LTB simulation ---
             p_inj = config['load_switch'] * status['p']
@@ -311,37 +342,31 @@ def run(case='ieee39_htb.xlsx', tf=20,
             # b) TDS
             ss.TDS.config.tf += config['t_step']
             # AGC
-            if status['k'] * config['t_step'] % intv_agc == 0:
-                ss.TurbineGov.set(src='paux0', idx=tgov_idx, attr='v', value=AGC_control * ACE_raw)
+            if status['k'] * config['t_step'] % config['intv_agc'] == 0:
+                ss.TurbineGov.set(src='paux0', idx=tgov_idx, attr='v', value=AGC_control * ACE.Raw)
             ss.TDS.run()
             if ss.exit_code != 0:
                 logger.warning("LTB simulation failed at %ds" % ss.TDS.config.tf)
                 break
             tc3 = time.time()  # record clock time
             # NOTE: tf_htb is the end time of last round
-            # update AGC PI Controller
-            ACE_integral = ACE_integral + ss.ACEc.ace.v.sum()
-            ACE_raw = -(Kp*ss.ACEc.ace.v.sum() + Ki*ACE_integral)
+            ACE.update(ss.ACEc.ace.v.sum())  # update AGC
             if tc3 - t0_htb > 2 * config['t_step']:
                 status['iter_fail'] += 1
-            status['tf'] = ss.TDS.config.tf  # LTB end time
-            status['tr'] = tc1 - t0_htb  # read time
-            status['tw'] = tc2 - tc1  # write time
-            status['tsim'] = tc3 - tc2  # write time
-            status['freq'] = f_bus  # freq of HTB bus
-            status['v'] = v_bus  # freq of HTB bus
-            # update HTB time
-        #     t0_htb += config['t_step']
+            # --- update status ---
+            # NOTE: update status dict
+            # LTB end time, read time, write time, sim time, freq, voltage
+            status = {**status, 'tf': ss.TDS.config.tf, 'tr': tc1 - t0_htb,
+          'tw': tc2 - tc1, 'tsim': tc3 - tc2, 'freq': f_bus, 'v': v_bus}
+
             status['iter_total'] += 1
-            flag_base = True
             #  --- record data ---
             for col in scols:
                 cs_num[status['k']-1, scols.index(col)] = status[col]
             # update counter base
-            if flag_base & (status['kr'] == 199):
+            if status['kr'] == 199:
                 status['kr'] = 10
                 kr0 = 10
-                flag_base = False
             # --- check if end ---
         except KeyboardInterrupt:
             logger.warning("Keyboard interrupt received. Exiting...")
